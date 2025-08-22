615f8b6ceca34c1407f7b78e99ecd7cdf6fdd4fd
Sinhularity <zs22017021@estudiantes.uv.mx>
2025-08-22 09:50:20 -0600

    GH pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site : 'https://Sinhularity.github.io',
+    base : 'minimalist-portfolio-json',
 });
